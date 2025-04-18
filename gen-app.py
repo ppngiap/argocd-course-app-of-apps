2fbc1fafbdc45e02eb0f30b1e9354e6ba0b9ba9d
tom phan <ppngiap@gmail.com>
2025-04-18 13:18:56 -0700

    project dcp

--- gen-app.py
+++ gen-app.py
@@ -1,19 +1,19 @@
 
 
-def app_str(wave: int, instance: int):
+def app_str(namespace: str, appname: str, wave: int):
     appstr = f"""
 apiVersion: argoproj.io/v1alpha1
 kind: Application
 metadata: 
-  name: app{wave}-{instance:02}
+  name: {appname}
   namespace: argocd
   annotations:
     argocd.argoproj.io/sync-wave: "{wave}"
 spec: 
   destination: 
-    namespace: dcp
+    namespace: {namespace} 
     server: "https://kubernetes.default.svc"
-  project: default
+  project: dcp
   source: 
     path: guestbook
     repoURL: "https://github.com/ppngiap/argocd-example-apps.git"
@@ -30,9 +30,11 @@ spec:
 def gen_app(waves: int, instances: int):
     for wave in range(0, waves):
         for instance in range(0, instances):
-            filename = f"directory-of-apps/app{wave}-{instance:02}.yaml"
+            appname = f"app{wave}-{instance:02}"
+            namespace = appname
+            filename = f"directory-of-apps/{appname}.yaml"
             with open(filename, "w") as f:
-                f.write(app_str(wave, instance))
+                f.write(app_str(namespace, appname, wave))
 
 
 gen_app(3,3)
